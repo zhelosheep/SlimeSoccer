e5d5c8a26861a93e4cf5225d74f1345ded1e557c
Tiffany Tjahjadi <ttjahjad@usc.edu>
2015-04-12 16:45:39 -0700

    Start mainmenu

--- src/view/MainMenu.java
+++ src/view/MainMenu.java
@@ -3,8 +3,28 @@ package view;
 import javax.swing.JFrame;
 
 public class MainMenu extends JFrame{
+	private static final long serialVersionUID = 1L;
 
 	MainMenu() {
+		setSize(800, 600);
+		setLocation(300,100);
+		
+		instantiateVariables();
+		addComponents();
+		addListeners();
+		setResizable(false);
+		setVisible(true);		
+	}
+	
+	private void instantiateVariables() {
+		
+	}
+	
+	private void addComponents() {
+		
+	}
+	
+	private void addListeners() {
 		
 	}
 }
