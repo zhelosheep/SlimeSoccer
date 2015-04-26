f5bce199f864ac3e437c0a94d5519d64b83a9752
Tiffany Tjahjadi <ttjahjad@usc.edu>
2015-04-25 22:26:44 -0700

    Add SpectateScreen

---
+++ src/view/SpectateScreen.java
@@ -0,0 +1,19 @@
+package view;
+
+import javax.swing.JFrame;
+
+import model.Slime;
+import model.SlimeCosmic;
+
+public class SpectateScreen extends JFrame {
+	private Slime slime1, slime2;
+	static public int groundLevel = 400;
+	static public int leftBoundary = 0, rightBoundary = 600;
+	
+	SpectateScreen() {
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
