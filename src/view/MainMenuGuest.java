8185b02f8eea2757037b3527cf0fff5feb250030
Tiffany Tjahjadi <ttjahjad@usc.edu>
2015-04-25 23:58:52 -0700

    Add SpectateScreen

--- src/view/MainMenuGuest.java
+++ src/view/MainMenuGuest.java
@@ -36,6 +36,7 @@ public class MainMenuGuest extends JFrame{
 	public BufferedReader sReader;
 	public PrintWriter sWriter;
 	private static JLabel spectateLabel;
+	private SpectateScreen spectateScreen;
 	
 	public MainMenuGuest() {
 		setSize(800, 600);
@@ -55,6 +56,7 @@ public class MainMenuGuest extends JFrame{
 		chatField = new JTextField(10);
 		backButton = new JButton("Back");
 		gameIDField = new JTextField(6);
+		spectateScreen = new SpectateScreen(true, this);
 	}
 	
 	private void addComponents() {
@@ -142,6 +144,18 @@ public class MainMenuGuest extends JFrame{
 				sWriter.flush();
 				chatArea.setText(chatArea.getText() + "\nGuest: " + chatField.getText());
 				chatField.setText("");
+			}
+		});
+		searchButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				spectateScreen.setVisible(true);
+				setVisible(false);
+			}
+		});
+		randomButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				spectateScreen.setVisible(true);
+				setVisible(false);
 			}
 		});
 		
